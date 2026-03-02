747826342e91f11206c5cf1ade35eabcdd55cef0
HarrisonKGr26 <yoshitime8251@gmail.com>
2026-03-02 10:04:32 -0700

    comment added on line 14 added a %d on line 13 to make the code work

--- main.c
+++ main.c
@@ -7,10 +7,10 @@ int main(void) {
     int second;
     int third;
     printf("My name is Harrison and I am taking Computer Science 10\n");
-    printf("please give me two numbers that i will calculate the avrage:");
+    printf("please give me two numbers that i will calculate the Sum:");
     scanf("%d %d", &first, &second);
     third = first + second;
-    printf("here is the avrage", third/2);
+    printf("here is the average: %d", third/2);
     //this is a push/change
     return 0;
 }
